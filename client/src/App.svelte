69b43f26cea8ab0f85f8d34bd0f2cbdc5a0a1e9a
Tony Gustafsson <mail@tonyg.se>
2019-07-25 15:53:57 +0200

    Making svelte client access couch with pouchdb

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,24 +1,64 @@
 <script>
-  export let clicked;
+    import { onMount } from 'svelte';
+    import PouchDB from 'pouchdb-browser';
 
-  let handleClick = () => {
-    clicked++;
-  };
+    let sites = [];
+    let isLoading = true;
+
+    let localDb = new PouchDB('db');
+    let remoteDb = new PouchDB('http://localhost:5984/lightshack');
+
+    localDb.replicate
+        .from(remoteDb, {
+            live: true,
+            retry: true
+        })
+        .on('change', async function(info) {
+            console.log('Updating with new data');
+            await update();
+        })
+        .on('error', function(err) {
+            console.log('Replication error:', err);
+        });
+
+    export let clicked;
+
+    let handleClick = () => {
+        clicked++;
+    };
+
+    async function update() {
+        const allDocs = await localDb.allDocs({
+            include_docs: true
+        });
+        sites = allDocs.rows.map(row => row.doc);
+        isLoading = false;
+    }
+
+    onMount(async () => {
+        await update();
+    });
 </script>
 
 <style>
-  .app {
-    background: black;
-    color: white;
-    user-select: none;
-  }
-  h1 {
-    color: red;
-  }
+    .app {
+        background: black;
+        color: white;
+        user-select: none;
+    }
+    h1 {
+        color: red;
+    }
 </style>
 
 <body class="app" on:click={handleClick}>
-  <h1>LightShack</h1>
+    <h1>LightShack</h1>
 
-  <p>Clicked {clicked} times.</p>
+    <p>Clicked {clicked} times.</p>
+
+    <ul>
+        {#each sites as site (site._id)}
+            <li>{site.url}: {site.speedIndex}</li>
+        {/each}
+    </ul>
 </body>
